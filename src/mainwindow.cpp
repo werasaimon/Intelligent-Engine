5bef55503542ed4757f80e4107d70e3f48331428
werasaimon <nicemankit@gmail.com>
2018-05-08 01:24:15 +0300

    chore:code

---
+++ src/mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    ui->widget->keyPressEvent(event);
+}
+
+void MainWindow::keyReleaseEvent(QKeyEvent *event)
+{
+    ui->widget->keyReleaseEvent(event);
+}

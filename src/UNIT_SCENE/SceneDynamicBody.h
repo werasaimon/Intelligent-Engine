ef302b3b7267cf6f27f1c60c52c602cede40f9bc
werasaimon <nicemankit@gmail.com>
2018-05-08 00:52:23 +0300

    test: unit tests

---
+++ src/UNIT_SCENE/SceneDynamicBody.h
@@ -0,0 +1,42 @@
+#ifndef SCENEDYNAMICBODY_H
+#define SCENEDYNAMICBODY_H
+
+#include <map>
+
+#include "SceneCamera.h"
+#include "iPhysics/IPhysicsEngine.h"
+#include "IGeometry/IGeometry.h"
+
+
+
+
+class SceneDynamicBody : public SceneCamera
+{
+
+    bool mPause;
+    IPhysics::IDynamicsWorld *mDynamicWorld;
+
+    std::map<IGeometry::IMeshModel* , int > mPhysMapIndexes;
+
+    std::vector<IPhysics::IRigidBody*>  mPhysicsBodies;
+    std::vector<IGeometry::IMeshModel*> mModels;
+
+
+public:
+    SceneDynamicBody();
+
+    bool initialization();
+    void render(float FrameTime);
+    void update();
+    void resize( float width , float height );
+
+    void mouseMove( float x , float y  , int button);
+    void mousePress( float x , float y , int button );
+    void mouseReleasePress( float x , float y , int button );
+    void mouseWheel( float delta );
+
+    void keyboard(int key );
+    void destroy();
+};
+
+#endif // SCENEDYNAMICBODY_H

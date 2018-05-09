412727bf4ebd0433547aa40a02792fbccad41b51
werasaimon <nicemankit@gmail.com>
2018-05-10 02:26:07 +0300

    Intelligent-Engine

--- src/IEngine/IEngine.h
+++ src/IEngine/IEngine.h
@@ -1,8 +1,12 @@
 #ifndef IENGINE_H
 #define IENGINE_H
 
+//---------------------------------//
+
 #include "iMath/IMaths.h"
 #include "iPhysics/IPhysicsEngine.h"
 #include "IGeometry/IGeometry.h"
+
+//---------------------------------//
 
 #endif // IENGINE_H
